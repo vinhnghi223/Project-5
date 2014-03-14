2a08f12dc4ddbefc9f77c1afbc71c8791b08e98d
sergio <sergey.kibish@gmail.com>
2014-03-14 14:52:58 +0200

    AddScreenActivity & add_screen; renamed pie_graph.png to .jpg, changed few lines in AndroidMainfest

---
+++ app/src/main/java/pl/byd/wsg/promand/project5/AddScreenActivity.java
@@ -0,0 +1,65 @@
+package pl.byd.wsg.promand.project5;
+
+import android.content.Intent;
+import android.hardware.Camera;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+
+/**
+ * Created by Sergey on 3/14/14.
+ */
+
+public class AddScreenActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_screen);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void openCategoryChooser(View v){
+        Intent intent = new Intent(this, CategoriesActivity.class);
+        startActivity(intent);
+    }
+
+    public void openProjectChooser(View v){
+        Intent intent = new Intent(this, ProjectsActivity.class);
+        startActivity(intent);
+    }
+
+    public void takePhoto(View v){
+        Intent intent = new Intent(this, Camera.class);
+        startActivity(intent);
+    }
+
+    public void submitExpense(View v){
+        Intent intent = new Intent(this, AddExpence.class);
+        startActivity(intent);
+    }
+}
