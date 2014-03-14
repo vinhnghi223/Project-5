df39bc0f14cc1b3136edc8256e4577d0350c51a5
MiguelSantos <mig.az.santos@gmail.com>
2014-03-14 14:12:27 +0100

    Login page done and menu page

---
+++ app/src/main/java/pl/byd/wsg/promand/project5/projectActivity.java
@@ -0,0 +1,17 @@
+package pl.byd.wsg.promand.project5;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by TOSHIBA on 14-03-2014.
+ */
+public class projectActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.project_screen);
+    }
+
+
+
+}
