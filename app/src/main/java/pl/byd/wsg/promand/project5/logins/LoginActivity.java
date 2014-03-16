67f9d00d25e67bfce21a7980ffda6060bc7e6c8e
MiguelSantos <mig.az.santos@gmail.com>
2014-03-17 00:40:53 +0100

    Deleted some xml's from src/main/res/menu  (they were wrongly created, shouldn't be there)

--- app/src/main/java/pl/byd/wsg/promand/project5/logins/LoginActivity.java
+++ app/src/main/java/pl/byd/wsg/promand/project5/logins/LoginActivity.java
@@ -3,7 +3,6 @@ package pl.byd.wsg.promand.project5.logins;
 import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
