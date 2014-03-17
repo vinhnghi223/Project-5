942b10b90fbc19803c62e77a5a76d86187fe54d3
martta <marttapawlowska@gmail.com>
2014-03-17 10:32:05 +0100

    Add the login validation

--- app/src/main/java/pl/byd/wsg/promand/project5/logins/LoginActivity.java
+++ app/src/main/java/pl/byd/wsg/promand/project5/logins/LoginActivity.java
@@ -4,9 +4,13 @@ import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import pl.byd.wsg.promand.project5.menus.MenuActivity;
 import pl.byd.wsg.promand.project5.R;
@@ -20,13 +24,34 @@ public class LoginActivity extends Activity {
         setContentView(R.layout.login_page);
         ActionBar actionBar = getActionBar();
         actionBar.hide();
+
+/* eddited by Marta 16.03*/
+
+        EditText et = (EditText) findViewById(R.id.editTextPIN);
+
+        Button butt = (Button) findViewById(R.id.buttonLogin);
     }
 
 
     public void enterApp(View v){
-        Intent intent = new Intent(this, MenuActivity.class);
-        startActivity(intent);
+
+        EditText et = (EditText) findViewById(R.id.editTextPIN);
+        String PIN =et.getText().toString();
+
+        et.getEditableText().toString();
+
+        if (PIN.equals("1234")) {
+            Intent intent = new Intent(this, MenuActivity.class);
+            startActivity(intent);
+        }
+
+        else {
+
+            Toast toast_login_failed = Toast.makeText(this, "Incorrect PIN - try again", Toast.LENGTH_SHORT);
+            toast_login_failed.setGravity(Gravity.CENTER, 0, 0);
+            toast_login_failed.show();
+        }
+        }
 
     }
 
-}
